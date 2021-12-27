cec4d4a343d983f6e09bae5f9e0b6e8f639bf6f3
BARADA <barada199222@gmail.com>
2021-12-27 23:27:15 +0300

    Scheduler.schedule is returning an id now. Add ability to cancel task using returned id from schedule call.

--- Scheduler.js
+++ Scheduler.js
@@ -1,7 +1,7 @@
 const MAXIMUM_DELAY = 2147483647; // maximum that is used for browsers
 const DEFAULT_DELAY = 86400000; // one day
 
-// To schedule a task, call the schedule method, pass there 1. Date, 2. callback function.
+// To schedule a task, call the schedule method, pass there 1: Date, 2: callback function. Return uniq Id for remove if task was scheduled.
 // To set maximum delay: 1. Pass value when creating new instance. 2. Pass value to setMaxDelay of Scheduler
 class Scheduler {
     // maxDelay = Number
@@ -10,13 +10,21 @@ class Scheduler {
             ? (maxDelay <= MAXIMUM_DELAY ? maxDelay : MAXIMUM_DELAY)
             : DEFAULT_DELAY
         ;
-        this._queue = []; // { at: Date, callback: function } always sorted by time
+        this._queue = []; // { at: Date, callback: function, id: string } always sorted by time
         this._timeoutId = null;
         this._wakeUpTime = null;
+        this._idMap = {};
     }
 
     _evaluateCompliance(position, date) {
         return this._queue[position].at.getTime() - date.getTime();
+    }
+
+    _generateRandomId() {
+        let id;
+        do id = ([1e5]+1e4).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+        while (this._idMap.hasOwnProperty(id));
+        return this._idMap[id] = id;
     }
 
     _findRelevantPlace(schedulingTime) {
@@ -38,7 +46,9 @@ class Scheduler {
         let finish = false;
         do {
             if (this._queue[0] && this._queue[0].at.getTime() <= new Date().getTime()) {
-                this._queue.shift().callback();
+                const task = this._queue.shift();
+                task.callback();
+                delete this._idMap[task.id];
             } else finish = true;
         } while (!finish);
         this._start();
@@ -46,6 +56,8 @@ class Scheduler {
 
     _resetTimer() {
         clearTimeout(this._timeoutId);
+        this._timeoutId = null;
+        this._wakeUpTime = null;
     }
 
     _restart() {
@@ -59,7 +71,7 @@ class Scheduler {
             if (this._maxDelay < delay) delay = this._maxDelay;
             this._wakeUpTime = new Date(new Date().getTime() + delay);
             this._timeoutId = setTimeout(this._callbackProcess.bind(this), delay);
-        }
+        } else this._resetTimer();
     }
 
     _isEarlier(date) {
@@ -70,8 +82,8 @@ class Scheduler {
         if (this._isEarlier(date)) this._restart();
     }
 
-    _placeSchedulingTask(date, callback) {
-        this._queue.splice(this._findRelevantPlace(date), 0, { at: date, callback });
+    _placeSchedulingTask(date, callback, id) {
+        this._queue.splice(this._findRelevantPlace(date), 0, { at: date, callback, id });
     }
 
     _getValidateTime(date) {
@@ -87,9 +99,18 @@ class Scheduler {
         if (!callback) throw new Error('Callback is missing');
         if (skdDate.getTime() <= new Date().getTime()) callback();
         else {
-            this._placeSchedulingTask(skdDate, callback);
+            const id = this._generateRandomId();
+            this._placeSchedulingTask(skdDate, callback, id);
             this._updateTimer(skdDate);
+            return id;
         }
+    }
+
+    cancelSchedule(id) {
+        if (!id) throw new Error('Id is missing');
+        this._queue = this._queue.filter(task => task.id != id);
+        delete this._idMap[id];
+        this._restart();
     }
 
     setMaxDelay(delay) {
